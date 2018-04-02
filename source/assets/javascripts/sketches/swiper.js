218ab2a0b6385364581c0dc7dcfb4a5b6be4c590
Shaurjya Banerjee <shaurjyab@gmail.com>
2018-04-02 16:42:04 -0700

    testing vertical swipes part 2

--- source/assets/javascripts/sketches/swiper.js
+++ source/assets/javascripts/sketches/swiper.js
@@ -22,8 +22,8 @@ module.exports =  function() {
 		  // document.body registers gestures anywhere on the page
 		  var hammer = new Hammer(document.body, options);
 		  hammer.get('swipe').set({
-		    //direction: Hammer.DIRECTION_ALL
-		    direction: Hammer.DIRECTION_VERTICAL
+		    direction: Hammer.DIRECTION_ALL
+		    //direction: Hammer.DIRECTION_VERTICAL
 		  });
 		 
 		  hammer.on("swipe", p.swiped); //tie event 'swipe' to function 'swiped'
