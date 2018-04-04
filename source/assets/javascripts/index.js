624bf0d596aa553d170b71960ba5e1a68e3ac7a9
Shaurjya Banerjee <shaurjyab@gmail.com>
2018-04-03 17:42:52 -0700

    changed back to snake

--- source/assets/javascripts/index.js
+++ source/assets/javascripts/index.js
@@ -2,17 +2,17 @@
 
 // Javascripts
 
-const goop = require('./sketches/maze_generator');
-const border_walker = require('./sketches/maze_generator');
+const goop = require('./sketches/gradient_snake');
+const border_walker = require('./sketches/gradient_snake');
 var bodyEl = document.getElementsByTagName('body')[0];
 
 if( Math.random() > 0.5) {
-  bodyEl.classList.add('maze_generator');
+  bodyEl.classList.add('gradient_snake');
   bodyReady();
 
 	goop()
 } else {
-  bodyEl.classList.add('maze_generator')
+  bodyEl.classList.add('gradient_snake')
   bodyReady();
 
 	border_walker()
