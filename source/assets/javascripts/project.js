1dd0364337e2b59a1fd17628755b504958b18829
Ashwin Vaswani <ashwinv11@gmail.com>
2018-04-08 21:48:18 -0700

    fix sculptures page, add game example

--- source/assets/javascripts/project.js
+++ source/assets/javascripts/project.js
@@ -17,33 +17,49 @@ var which_sketch = Math.random();
 
 console.log(which_sketch);
 
-if (which_sketch < 0.25) {
-  bodyEl.classList.add('flowfield_inverted');
-  bodyReady();
-
-	flowfield_inverted()
-}
-
-else if (which_sketch >= 0.25 && which_sketch < 0.50) {
-  bodyEl.classList.add('border');
-  bodyReady();
-
-	border_walker()
-}
-
-else if (which_sketch >= 0.50  && which_sketch < 0.75) {
+if(bodyEl.classList.contains('projects')) {
   bodyEl.classList.add('grid_goop');
   bodyReady();
 
-	grid_goop()
-}
-
-else if (which_sketch >= 0.75) {
-  bodyEl.classList.add('border_inverted');
+  grid_goop()
+} else if (bodyEl.classList.contains('games_maze_generator')) {
+  bodyEl.classList.add('grid_goop');
   bodyReady();
 
-	border_walker_inverted()
+  maze_generator()
 }
+
+else {
+  if (which_sketch < 0.25) {
+    bodyEl.classList.add('flowfield_inverted');
+    bodyReady();
+
+    flowfield_inverted()
+  }
+
+  else if (which_sketch >= 0.25 && which_sketch < 0.50) {
+    bodyEl.classList.add('border');
+    bodyReady();
+
+    border_walker()
+  }
+
+  else if (which_sketch >= 0.50  && which_sketch < 0.75) {
+    bodyEl.classList.add('grid_goop');
+    bodyReady();
+
+    grid_goop()
+  }
+
+  else if (which_sketch >= 0.75) {
+    bodyEl.classList.add('border_inverted');
+    bodyReady();
+
+    border_walker_inverted()
+  }
+}
+
+
 
 function bodyReady() {
   bodyEl.classList.remove('unready');
