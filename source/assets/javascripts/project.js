4f0160a195b7c90d4f878bba91598a86ae616e02
Shaurjya Banerjee <shaurjyab@gmail.com>
2018-04-10 11:20:23 -0700

    added comments, made new game pages

--- source/assets/javascripts/project.js
+++ source/assets/javascripts/project.js
@@ -1,4 +1,3 @@
-// Stylesheets
 import '../stylesheets/project.scss';
 
 const flowfield               = require('./sketches/flowfield');
@@ -12,50 +11,65 @@ const feedbake_snake          = require('./sketches/feedbake_snake');
 const grid_goop               = require('./sketches/goop1');
 
 var bodyEl = document.getElementsByTagName('body')[0];
-
 var which_sketch = Math.random();
 
-console.log(which_sketch);
-
+//This is the sketch that is run on the sculptures page,
+//as identified by the 'projects' class
 if(bodyEl.classList.contains('projects')) {
   bodyEl.classList.add('grid_goop');
   bodyReady();
+  grid_goop();
+} 
 
-  grid_goop()
-} else if (bodyEl.classList.contains('games_maze_generator')) {
-  bodyEl.classList.add('grid_goop');
+//GAME PAGES ------------------------------------------------------------------------
+//This is the sketch run on the games/maze generator page
+else if (bodyEl.classList.contains('games_maze_generator')) {
+  bodyEl.classList.add('maze_generator');
   bodyReady();
-
-  maze_generator()
+  maze_generator();
 }
+
+//This is the sketch run on the games/simple_snake page
+else if (bodyEl.classList.contains('games_simple_snake')) {
+  bodyEl.classList.add('simple_snake');
+  bodyReady();
+  gradient_snake();
+}
+
+//This is the sketch run on the games/simple_snake page
+else if (bodyEl.classList.contains('games_feedbake_snake')) {
+  bodyEl.classList.add('feedbake_snake');
+  bodyReady();
+  feedbake_snake();
+}
+
+//PROJECT SKETCHES ------------------------------------------------------------------
+//These are the sketches that are chosen randomly on each 
+//individual project page
 
 else {
   if (which_sketch < 0.25) {
     bodyEl.classList.add('flowfield_inverted');
     bodyReady();
-
-    flowfield_inverted()
+    flowfield_inverted();
   }
 
   else if (which_sketch >= 0.25 && which_sketch < 0.50) {
     bodyEl.classList.add('border');
     bodyReady();
-
-    border_walker()
+    border_walker();
   }
 
   else if (which_sketch >= 0.50  && which_sketch < 0.75) {
-    bodyEl.classList.add('grid_goop');
+    bodyEl.classList.add('flowfield');
     bodyReady();
-
-    grid_goop()
+    flowfield();
   }
 
   else if (which_sketch >= 0.75) {
     bodyEl.classList.add('border_inverted');
     bodyReady();
-
-    border_walker_inverted()
+    border_walker_inverted();
   }
 }
 
